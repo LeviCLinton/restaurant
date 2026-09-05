3611006560e1dda901fd8a2b658a3ae1c67e4e49
LCN <69968373+LeviCLinton@users.noreply.github.com>
2026-09-05 09:07:54 +0300

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/restaurant/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
